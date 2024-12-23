e31c59c94b8ece7d8732e2611a7ae110f2c30fac
Stoyan Krastev (skkdevcraft) <163518570+skkdevcraft@users.noreply.github.com>
2024-12-23 16:32:48 +0000

    added github link

--- packages/demo/src/components/ExampleLayout.jsx
+++ packages/demo/src/components/ExampleLayout.jsx
@@ -36,10 +36,19 @@ export function ExampleLayout({ examples }) {
       <div className="flex">
         {/* Sidebar */}
         <div className={clsx(
-          'w-64 h-screen fixed left-0 p-4 border-r overflow-y-auto',
+          'w-60 h-screen fixed left-0 p-4 border-r overflow-y-auto',
           darkMode ? 'border-gray-700 bg-gray-800' : 'border-gray-200 bg-gray-50'
         )}>
-          <h1 className="text-xl font-bold mb-6">Examples</h1>
+          <h1 className="text-xl font-bold mb-6">Reveraie DataGrid</h1>
+          <nav className="space-y-2">
+            <a href="https://github.com/reveraie/datagrid" target="_blank" rel="noreferrer" className="w-full flex items-center space-x-2 px-4 py-2 rounded-lg transition-colors hover:bg-gray-100 dark:hover:bg-gray-700">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                <path fillRule="evenodd" d="M12 2C6.477 2 2 6.477 2 12c0 4.418 2.865 8.166 6.839 9.489.5.092.682-.217.682-.483 0-.237-.01-1.021-.014-1.852-2.782.603-3.369-1.342-3.369-1.342-.454-1.155-1.11-1.463-1.11-1.463-.908-.621.069-.608.069-.608 1.004.07 1.533 1.032 1.533 1.032.892 1.528 2.341 1.087 2.91.831.091-.647.35-1.087.637-1.337-2.221-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.272.098-2.65 0 0 .84-.269 2.75 1.025A9.564 9.564 0 0112 6.8c.85.004 1.705.116 2.505.34 1.91-1.294 2.75-1.025 2.75-1.025.545 1.378.202 2.397.1 2.65.64.7 1.028 1.592 1.028 2.683 0 3.841-2.337 4.687-4.565 4.936.359.31.679.923.679 1.86 0 1.343-.012 2.425-.012 2.753 0 .267.18.579.688.481C19.138 20.164 22 16.418 22 12c0-5.523-4.477-10-10-10z" clipRule="evenodd" />
+              </svg>
+              <span className="font-semibold">View on Github</span>
+            </a>
+          </nav>
+          <h1 className="text-xl font-bold mb-6 pt-8">Examples</h1>
           <nav className="space-y-2">
             {examples.map((example, index) => (
               <button
