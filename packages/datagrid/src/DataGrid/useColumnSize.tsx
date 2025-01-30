2b2c92107bbcb2b5de6c541cb2c7b7351b9e261f
Stoyan Krastev (skkdevcraft) <163518570+skkdevcraft@users.noreply.github.com>
2025-01-30 06:25:30 +0000

    chore: align tsconfig with reveraie tsconfig

--- packages/datagrid/src/DataGrid/useColumnSize.tsx
+++ packages/datagrid/src/DataGrid/useColumnSize.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useCallback, useRef } from 'react';
 
 function widthValue(size: string | number | undefined) {
--- packages/datagrid/src/__tests__/DataGrid.test.tsx
+++ packages/datagrid/src/__tests__/DataGrid.test.tsx
@@ -1,4 +1,4 @@
-import React, { act } from 'react';
+import { act } from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
